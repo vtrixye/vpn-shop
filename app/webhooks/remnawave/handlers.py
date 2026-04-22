8abd4def8d216fff6da31baf3df9094687e913e1
vtrixye <p.eremchenko@gmail.com>
2026-04-22 14:29:32 +0300

    await fix

--- app/webhooks/remnawave/handlers.py
+++ app/webhooks/remnawave/handlers.py
@@ -10,4 +10,4 @@ logger = get_logger(__name__)
 @remnawave_handler("user.created")
 async def user_created(data):
     logger.info("remnawave user_created")
-    bot.send_message(chat_id=1253142390, text=f"Создан пользователь \n{data}")
+    await bot.send_message(chat_id=1253142390, text=f"Создан пользователь \n{data}")
