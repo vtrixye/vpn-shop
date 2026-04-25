f8f40a2cceff6b5efcbab2260f91264989900730
vtrixye <p.eremchenko@gmail.com>
2026-04-25 23:36:44 +0300

    keyboard fix

--- app/telegram/keyboards/users.py
+++ app/telegram/keyboards/users.py
@@ -6,22 +6,17 @@ from database.models import User
 async def main_menu(session: AsyncSession, id: int) -> InlineKeyboardMarkup:
     user = await session.get(User, id)
     keyboard = InlineKeyboardBuilder()
-    
-    keyboard.add(InlineKeyboardButton(text="Попробовать бесплатно", callback_data="trial_sub"))
 
-    keyboard.add(
-        InlineKeyboardButton(text="Купить", callback_data="buy_sub"),
+    keyboard.row(InlineKeyboardButton(text="Попробовать бесплатно", callback_data="trial_sub"))
+    
+    keyboard.row(InlineKeyboardButton(text="Купить", callback_data="buy_sub"))
+    keyboard.row(
         InlineKeyboardButton(text="Профиль", callback_data="profile"),
-        InlineKeyboardButton(text="Поддержка", callback_data="help"),
+        InlineKeyboardButton(text="Поддержка", callback_data="help")
     )
-    keyboard.adjust(1, 2)
-    if len(keyboard) == 4:
-        keyboard.adjust(1, 1, 2)
     
     if user.admin:
-        keyboard.add(InlineKeyboardButton(text="Админ панель", callback_data="admin_menu"))
-        keyboard.adjust(1, 2, 1)
-        if len(keyboard) == 5:
-            keyboard.adjust(1, 1, 2, 1)
+        keyboard.row(InlineKeyboardButton(text="Админ панель", callback_data="admin_menu"))
+    
     return keyboard.as_markup()
 
