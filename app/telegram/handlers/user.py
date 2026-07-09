1e303ce40cc36279e6f88bdfbcd399186b96b7cb
vtrixye <p.eremchenko@gmail.com>
2026-07-09 15:11:27 +0300

    fix alert test

--- app/telegram/handlers/user.py
+++ app/telegram/handlers/user.py
@@ -78,7 +78,7 @@ async def sub_dev(callback: CallbackQuery, session: AsyncSession, state: FSMCont
 @user_router.callback_query(F.data.startswith("sub:"))
 async def sub_menu(callback: CallbackQuery, session: AsyncSession, state: FSMContext):
     #убрать потом
-    return await callback.answer("Разработчики sdk допустили ошибку в обновлении\n. Откат невозможен. Ждем фикс :(", show_alert=True)
+    return await callback.answer("Разработчики sdk допустили ошибку в обновлении.\nОткат невозможен. Ждем фикс :(", show_alert=True)
     await state.clear()
 
     short_uuid = callback.data.split(":")[1]
